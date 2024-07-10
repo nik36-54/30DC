9b6c538d67de447ce85836ee315cd83ec666ae89
Niraj <niraj3654@gmail.com>
2024-07-11 00:36:00 +0530

    to-do init

---
+++ JS Mastery/JS DOM concept/to_do/script.js
@@ -0,0 +1,5 @@
+const todoInput = document.getElementById('todo-input');
+const add = document.getElementById('add');
+const del = document.getElementById('delete');
+const todoList = document.getElementById('todo-list');
+
